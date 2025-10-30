e554711bfa38e82d62b9939cd76c28803d1ba549
WGrot <117313232+WGrot@users.noreply.github.com>
2025-10-30 18:47:21 +0100

    first few rows for dashboard

---
+++ RestaurantApp/RestaurantApp.Blazor/Pages/Dashboard/RestaurantDashboard.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using RestaurantApp.Shared.Models;
+
+namespace RestaurantApp.Blazor.Pages.Dashboard;
+
+public partial class RestaurantDashboard : ComponentBase
+{
+    public Restaurant loadedRestaurant { get; set; } = new Restaurant();
+    private DateTime currentDate = DateTime.Now;
+    private System.Threading.Timer? timer;
+
+    protected override void OnInitialized()
+    {
+        timer = new System.Threading.Timer(_ =>
+        {
+            InvokeAsync(() =>
+            {
+                currentDate = DateTime.Now;
+                StateHasChanged();
+            });
+        }, null, 0, 1000);
+    }
+
+    public void Dispose()
+    {
+        timer?.Dispose();
+    }
+}
